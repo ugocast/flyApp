c3eac33c10bc1b5353dd9c96a21bd6b8251cbc69
Hugo Castellon <h.castellon.guerrero@gmail.com>
2016-03-19 21:52:27 -0300

    changes in plugins

--- www/js/services.js
+++ www/js/services.js
@@ -2,7 +2,7 @@ angular.module('starter.services', ['ngResource'])
 
 .factory('Places', ['$resource',
   function($resource) {
-    return $resource('http://0.0.0.0:3000/api/FirstLevels?filter[include]=places&filter[where][google_place_id]=:place_id', {}, {
+    return $resource('https://apiflyapp-flyapp.rhcloud.com/api/Language_Countries/es_cl/first_Levels?filter[include]=places&filter[where][google_place_id]=:place_id', {}, {
       query: {
         method: 'GET',
         headers: {
@@ -14,7 +14,7 @@ angular.module('starter.services', ['ngResource'])
   }
 ]).factory('Place', ['$resource',
   function($resource) {
-    return $resource('http://0.0.0.0:3000/api/places/:placeId?filter[include]=fishes&filter[include]=flies&filter[include]=access&filter[include]=advertisements&filter[include]=insects', {}, {
+    return $resource('https://apiflyapp-flyapp.rhcloud.com/api/places/:placeId?filter[include]=fishes&filter[include]=flies&filter[include]=access&filter[include]=advertisements&filter[include]=insects', {}, {
       query: {
         method: 'GET',
         headers: {
